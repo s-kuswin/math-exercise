7dae1d47f77a7f32e7b97eecafdd483322da2f11
kuswin <1398985433@qq.com>
2020-09-13 22:11:16 +0800

    环境

---
+++ miniprogram/data/class.js
@@ -0,0 +1,22 @@
+var local_database = [
+  {
+    title:"一年级",
+    postId:'0'
+  },
+  {
+    title:"二年级",
+    postId:'1'
+  },
+  {
+    title:"三年级",
+    postId:'2'
+  },
+  {
+    title:"四年级",
+    postId:'3'
+  }
+]
+
+module.exports = {
+  classList: local_database
+}
---
+++ miniprogram/pages/test/test.js
@@ -0,0 +1,82 @@
+// miniprogram/pages/test/test.js
+var classData = require('../../data/class.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  onTap: function (event) {
+    wx.navigateTo({
+        url:"../home/home?postId=" + event.target.dataset.id
+    });
+    console.log(event)
+    // wx.switchTab({
+    //     url: "../posts/post"
+    // });
+  
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(classData);
+    
+    this.setData({
+      classData:classData.classList
+     });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
